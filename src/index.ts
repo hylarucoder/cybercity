dde21a97c9aad58a5776cd93e44bf844beaf49f7
twocucao <twocucao@gmail.com>
2021-04-07 18:58:18 +0800

    add scheduled basic

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,97 @@
 import "reflect-metadata";
 import "module-alias/register";
+import * as repl from "repl";
+import { Command } from "commander";
 
 import { createApp } from "./app";
+import { initWorker } from "@/tasks/worker";
+import { addJobs } from "@/tasks/queue";
 
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3000;
 
-const app = createApp();
+function startServer() {
+  const app = createApp();
+  app.listen(FASTIFY_PORT, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    app.log.info(`🚀  Server listening at ${address}`);
+    app.printAllRoutes();
+  });
+}
 
-app.listen(FASTIFY_PORT, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  app.log.info(`🚀  Server listening at ${address}`);
-  app.printAllRoutes()
-});
+function startBeatServer() {
+  const app = createApp({
+    type: "beat",
+  });
+  addJobs();
+}
+
+function startWorker() {
+  const app = createApp();
+  initWorker();
+}
+
+const BANNER = `
+===================================================================
+
+ _______       ___   ____    ____  __  .__   __.   ______  __  
+|       \\     /   \\  \\   \\  /   / |  | |  \\ |  |  /      ||  | 
+|  .--.  |   /  ^  \\  \\   \\/   /  |  | |   \\|  | |  ,----'|  | 
+|  |  |  |  /  /_\\  \\  \\      /   |  | |  . \`  | |  |     |  | 
+|  '--'  | /  _____  \\  \\    /    |  | |  |\\   | |  \`----.|  | 
+|_______/ /__/     \\__\\  \\__/     |__| |__| \\__|  \\______||__| 
+
+
+===================================================================
+
+                             极速开发框架，拷贝在身边，一用好多年
+                                              -- since 2021
+`;
+
+function startShell() {
+  const app = createApp();
+  app.ready((error) => {
+    app.log.info(BANNER);
+    if (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return false;
+    }
+    const replInstance = repl.start();
+    replInstance.context.app = app;
+    Object.assign(replInstance.context, app.prisma);
+  });
+}
+
+const program = new Command();
+program
+  .command("serve")
+  .description("后端服务")
+  .action(() => {
+    startServer();
+  });
+
+program
+  .command("beat")
+  .description("调度服务")
+  .action(() => {
+    startBeatServer();
+  });
+
+program
+  .command("worker")
+  .description("Worker服务")
+  .action(() => {
+    startWorker();
+  });
+
+program
+  .command("shell")
+  .description("Repl调试命令")
+  .action(() => {
+    startShell();
+  });
+
+program.parse(process.argv);
